20a94d2335ec2221937bcde3201d4f98caf34b28
heuzin <matheusqs.arq@gmail.com>
2021-02-01 18:17:02 -0500

    sorting todos by completed

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,6 +1,6 @@
 const todos = [{
     text: 'Workout',
-    completed: false,
+    completed: true,
 }, {
     text: 'Study',
     complete: true
@@ -9,12 +9,29 @@ const todos = [{
     complete: false
 }]
 
+const summary = document.createElement('h1')
+summary.textContent = `You have ${todos.length} todos left`
+document.querySelector('#todo-summary').appendChild(summary)
+
+const sortedTodos = (todos) => {
+    todos.sort((a, b) => {
+        if (a.complete && !b.complete) {
+            return 1
+        } else if (!a.complete && b.complete) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+}
+
 const showTodos = (todos) => {
     todos.forEach((todo) => {
         const newEl = document.createElement('p')
         newEl.textContent = todo.text
         document.querySelector('#todo-list').appendChild(newEl)
-    })
+    })  
 }
 
+sortedTodos(todos)
 showTodos(todos)
