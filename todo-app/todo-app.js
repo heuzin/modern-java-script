9e5548b8cb13f13b105365e8ad9c13cb295143c8
heuzin <matheusqs.arq@gmail.com>
2021-02-01 18:04:14 -0500

    showing todos on page

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,1 +1,20 @@
-console.log('workingsa')
+const todos = [{
+    text: 'Workout',
+    completed: false,
+}, {
+    text: 'Study',
+    complete: true
+}, {
+    text: 'Play',
+    complete: false
+}]
+
+const showTodos = (todos) => {
+    todos.forEach((todo) => {
+        const newEl = document.createElement('p')
+        newEl.textContent = todo.text
+        document.querySelector('#todo-list').appendChild(newEl)
+    })
+}
+
+showTodos(todos)
